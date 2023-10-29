f093c4f06247594495ad35db4cb951536ff8d6ef
Adrian <marczaka.95@gmail.com>
2023-10-29 12:49:47 +0100

    basic structure and working json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { Navigation } from './components/Navigation';
+import { NotFound } from './pages/NotFound';
+import { Home } from './pages/Home';
+import { EmployeeList } from './pages/EmployeeList';
+import { EmployeeDetails } from './pages/EmployeeDetails';
+import { useEffect } from 'react';
 
 function App() {
+  const getWorkers = async () => {
+    try {
+      const data = await fetch("http://localhost:5000/workers");
+      if (!data.ok) throw new Error("Something goes wrong");
+      const workers = await data.json();
+      console.log(workers, " pobrano dane pracownika");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getWorkers();
+  }, []);
   return (
+    <> 
+    <Navigation/>
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/employees" element={<EmployeeList/>}/>
+        <Route path ="/employees/:id" element={<EmployeeDetails/>}/>
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </div>
+    </>
+   
   );
 }
 
