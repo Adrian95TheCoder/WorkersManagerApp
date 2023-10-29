e46de750f61f51b7d7bbfaa2e73845115d8229b1
Kacper Strzelecki <kacper.a.strzelecki@gmail.com>
2023-10-29 14:50:32 +0100

    add to component addEmployye

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.scss';
-import { Navigation } from './components/Navigation';
-import { NotFound } from './pages/NotFound';
-import { Home } from './pages/Home';
-import { EmployeeList } from './pages/EmployeeList';
-import { EmployeeDetails } from './pages/EmployeeDetails';
-import { useEffect } from 'react';
+import { Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { Navigation } from "./components/Navigation";
+import { NotFound } from "./pages/NotFound";
+import { Home } from "./pages/Home";
+import { EmployeeList } from "./pages/EmployeeList";
+import { EmployeeDetails } from "./pages/EmployeeDetails";
+import { useEffect } from "react";
+import { AddEmployee } from "./pages/AddEmployee";
 
 function App() {
   const getWorkers = async () => {
@@ -22,18 +23,18 @@ function App() {
     getWorkers();
   }, []);
   return (
-    <> 
-    <Navigation/>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/employees" element={<EmployeeList/>}/>
-        <Route path ="/employees/:id" element={<EmployeeDetails/>}/>
-        <Route path="*" element={<NotFound/>}/>
-      </Routes>
-    </div>
+    <>
+      <Navigation />
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/employees" element={<EmployeeList />} />
+          <Route path="/employees/addEmployee" element={<AddEmployee />} />
+          <Route path="/employees/:id" element={<EmployeeDetails />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
     </>
-   
   );
 }
 
--- src/pages/EmployeeList/index.tsx
+++ src/pages/EmployeeList/index.tsx
@@ -1,6 +1,9 @@
-export const EmployeeList = () =>{
-    return(
-        <>
-        </>
-    )
-}
+import { Link } from "react-router-dom";
+
+export const EmployeeList = () => {
+  return (
+    <>
+      <Link to={"/employees/addEmployee"}></Link>
+    </>
+  );
+};
