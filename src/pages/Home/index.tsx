fbebd57c0a378d953bb6d8b1aa42751f2a968bc3
Artur Poznanski <apoznan5@gmail.com>
2023-10-31 21:17:57 +0100

    login page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,46 @@
-export const Home = () =>{
-    return(
-        <>
-        </>
-    )
-}
+import { useNavigate } from "react-router-dom";
+import "./Home.scss"
+import { ChangeEvent } from "react";
+export const Home = () => {
+    const navigate = useNavigate();
+    const signIn = () => {
+        console.log("test");
+        navigate("/employees");
+    }
+
+    const handleLoginInput = (event: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = event.target;
+        console.log("test");
+    }  
+
+  return (
+    <>
+      <div className="LoginMain">
+      <div className="Login_box">
+        <form action="">
+          <h2>Login Page</h2>          
+          <div className="labelBox">
+            <label htmlFor="firstName">
+              Login:
+              <input type="text" 
+                    id="login" 
+                    placeholder="Enter login"
+                    onChange={handleLoginInput} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor="password">
+              Password:
+              <input type="password" 
+              id="password" 
+              placeholder="Enter password" 
+              onChange={handleLoginInput}/>
+            </label>
+          </div>
+          <button onClick={signIn}>Sign In</button>
+        </form>
+      </div>
+    </div>
+    </>
+  );
+};
