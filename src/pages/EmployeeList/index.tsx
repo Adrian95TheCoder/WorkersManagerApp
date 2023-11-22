339427e1b6fad84ab74dca46a8ab4edf7c19189f
Artur Poznański <apoznan5@gmail.com>
2023-11-23 00:15:02 +0100

    access denied for not logged

--- src/pages/EmployeeList/index.tsx
+++ src/pages/EmployeeList/index.tsx
@@ -6,10 +6,12 @@ import { useTranslation } from "react-i18next";
 import { EmployeeBox } from "../../components/EmployeeBox";
 import { InputSearchBox } from "../../components/InputSearchBox";
 import { DisplaySortBox } from "../../components/DisplaySortBox";
+import { UserContext } from "../../components/context/UserContext";
 
 export const EmployeeList = () => {
   const { employeeList, curPage, previousPage, nextPage } =
     useContext(EmployeeContext);
+  const { token } = useContext(UserContext);
 
   /*  search  */
   // const { search } = useLocation();
@@ -19,64 +21,77 @@ export const EmployeeList = () => {
   const { t } = useTranslation();
 
   const renderTable = () => {
-    console.log("renderTable");
-    console.log("curPage");
+    //console.log("renderTable");
+    //console.log("curPage");
     return (
       <div className="EmployeeList">
-        <h2 className="EmployeeList__h2">{t("employeeList")}</h2>
+        {token ? (
+          <>
+            <h2 className="EmployeeList__h2">{t("employeeList")}</h2>
 
-        <DisplaySortBox />
+            <DisplaySortBox />
 
-        <InputSearchBox />
+            <InputSearchBox />
 
-        <table className="EmployeeList__table3">
-          <thead>
-            <tr>
-              <th className="EmployeeList__employee_lp">{t("nr")}</th>
-              <th className="EmployeeList__employee_id">Id</th>
-              <th className="EmployeeList__employee_firstName">
-                {t("firstName")}
-              </th>
-              <th className="EmployeeList__employee_lastName">
-                {t("lastName")}
-              </th>
-              <th className="EmployeeList__employee_workplace">
-                {t("workplace")}
-              </th>
-              <th className="EmployeeList__employee_salary">{t("salary")}</th>
-              <th className="EmployeeList__details">{t("detailsButtons")}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {employeeList.map(
-              ({ id, firstName, lastName, salary, workplace }, index) => (
-                <EmployeeBox
-                  key={id}
-                  id={id}
-                  firstName={firstName}
-                  lastName={lastName}
-                  salary={salary}
-                  workplace={workplace}
-                  index={index}
-                />
-              )
-            )}
-          </tbody>
-          {t("displayPage")}: {curPage}
-        </table>
+            <table className="EmployeeList__table3">
+              <thead>
+                <tr>
+                  <th className="EmployeeList__employee_lp">{t("nr")}</th>
+                  <th className="EmployeeList__employee_id">Id</th>
+                  <th className="EmployeeList__employee_firstName">
+                    {t("firstName")}
+                  </th>
+                  <th className="EmployeeList__employee_lastName">
+                    {t("lastName")}
+                  </th>
+                  <th className="EmployeeList__employee_workplace">
+                    {t("workplace")}
+                  </th>
+                  <th className="EmployeeList__employee_salary">
+                    {t("salary")}
+                  </th>
+                  <th className="EmployeeList__details">
+                    {t("detailsButtons")}
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {employeeList.map(
+                  ({ id, firstName, lastName, salary, workplace }, index) => (
+                    <EmployeeBox
+                      key={id}
+                      id={id}
+                      firstName={firstName}
+                      lastName={lastName}
+                      salary={salary}
+                      workplace={workplace}
+                      index={index}
+                    />
+                  )
+                )}
+              </tbody>
+              {t("displayPage")}: {curPage}
+            </table>
 
-        <button className="EmployeeList__addEmployee" onClick={previousPage}>
-          {t("previousPage")}
-        </button>
-        <button className="EmployeeList__addEmployee" onClick={nextPage}>
-          {t("nextPage")}
-        </button>
+            <button
+              className="EmployeeList__addEmployee"
+              onClick={previousPage}
+            >
+              {t("previousPage")}
+            </button>
+            <button className="EmployeeList__addEmployee" onClick={nextPage}>
+              {t("nextPage")}
+            </button>
 
-        <Link to={"/employees/addEmployee"}>
-          <button className="EmployeeList__addEmployee">
-            {t("addEmployee")}
-          </button>
-        </Link>
+            <Link to={"/employees/addEmployee"}>
+              <button className="EmployeeList__addEmployee">
+                {t("addEmployee")}
+              </button>
+            </Link>
+          </>
+        ) : (
+          <p className="EmployeeList__accessDenied">{t("AccessDenied")}</p>
+        )}
       </div>
     );
   };
